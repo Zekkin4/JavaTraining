f7c6724ebcd631033adef93594ef352f73320500
Zekkin4 <125632035+Zekkin4@users.noreply.github.com>
2023-02-18 10:41:54 +0530

    asignment

---
+++ a2.java
@@ -0,0 +1,14 @@
+package Assignment;
+
+public class a2 {
+	static long l;
+	static int i;
+	static short s;
+	static float f;
+	static double d;
+	static char c;
+	public static void main(String[] args) {
+	
+		System.out.println(i+"\n"+l+"\n"+s+"\n"+f+"\n"+d+"\n"+c);
+	}
+}
